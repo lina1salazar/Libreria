c6a232845c7917e08c95d9a1bcbd18fcbce50ade
Lina Salazar <salazarlina35@gmail.com>
2025-06-06 16:07:20 -0500

    Creando componente inicio y ajustes al index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'libreria-angular';
+  title = 'Bookish';
 }
